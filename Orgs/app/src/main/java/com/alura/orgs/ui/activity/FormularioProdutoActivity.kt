3c5292a3710af347efb7954bce26ad2e100d05ab
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2023-03-08 17:44:05 -0300

    Recursos do Android Studio e configuração de build

---
+++ Orgs/app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,7 @@
+package com.alura.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+}
--- Orgs/app/src/main/java/com/alura/orgs/ui/activity/MainActivity.kt
+++ Orgs/app/src/main/java/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.alura.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.alura.orgs.R
 import com.alura.orgs.model.Produto
 import com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
             context = this, produtos = listOf(
