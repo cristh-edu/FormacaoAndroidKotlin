dd95556a3515f7376b87fa5a8e2a12ec014a01b4
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2023-03-08 18:47:54 -0300

    Implementando Formulário

--- Orgs/app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
+++ Orgs/app/src/main/java/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,36 @@
 package com.alura.orgs.ui.activity
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.alura.orgs.R
+import com.alura.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val botaoSalvar = findViewById<Button>(R.id.button_salvar)
+        botaoSalvar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val campoNome = findViewById<EditText>(R.id.nome)
+                val nome = campoNome.text.toString()
+                val campoDescricao = findViewById<EditText>(R.id.descricao)
+                val descricao = campoDescricao.text.toString()
+                val campoValor = findViewById<EditText>(R.id.valor)
+                val valorEmTexto = campoValor.text.toString()
+                val valor = if(valorEmTexto.isBlank())
+                    BigDecimal.ZERO
+                else
+                    BigDecimal(valorEmTexto)
+
+                val produtoNovo = Produto(nome, descricao, valor)
+                Log.i("FormularioProduto", "onCreate: $produtoNovo")
+            }
+        })
+    }
 }
