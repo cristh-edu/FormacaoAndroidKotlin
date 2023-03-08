6efea78af4c85142d965ae02d4e22933e453838c
Cristhian Eduardo <contato.cristhian.slopes@gmail.com>
2023-03-08 15:30:05 -0300

    Criando o primeiro layout da activity

--- Orgs/app/src/main/java/com/alura/orgs/MainActivity.kt
+++ Orgs/app/src/main/java/com/alura/orgs/MainActivity.kt
@@ -2,11 +2,11 @@ package com.alura.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 }
